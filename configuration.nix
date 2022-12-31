06f91e6f47a0fe0542a4ba43cdb4c7edbcc216b1
SolidRhino <SolidRhino@users.noreply.github.com>
2022-12-31 16:57:47 +0100

    Add Tailscale

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,12 @@
 { pkgs, config, lib, inputs, ... }:
 {
-  environment.systemPackages = with pkgs; [ vim git ];
-  services.openssh.enable = true;
-  networking.hostName = "server";
+  environment.systemPackages = with pkgs; [ vim git tailscale];
+  services = {
+    openssh = {
+    tailscale = {
+      enable = true;
+    };
+  };
   users = {
     users.myUsername = {
       password = "myPassword";
@@ -11,13 +15,44 @@
     };
   };
   networking = {
-    interfaces."wlan0".useDHCP = true;
-    wireless = {
-      interfaces = [ "wlan0" ];
+    interfaces."eth0".useDHCP = true;
+    firewall = {
+      allowedUDPPorts = [
+        41641
+      ];
       enable = true;
-      networks = {
-        networkSSID.psk = "password";
-      };
+      trustedInterfaces = [
+        "tailscale0"
+      ];
     };
+    hostName = "server";
+  };
+
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey tskey-auth-kGtZkF3CNTRL-MpPL5dqqLRPJ5w2TzFjbYP574iAe3hqF
+    '';
   };
 }
