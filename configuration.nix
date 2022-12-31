33a6c2fb56a21721076374b6e872c8edb21ad644
SolidRhino <SolidRhino@users.noreply.github.com>
2022-12-31 17:03:55 +0100

    Auto optimise and cleanup nixstore

--- configuration.nix
+++ configuration.nix
@@ -18,6 +18,20 @@
 
   services.hardware.argonone.enable = true;
 
+  nix = {
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+    # Free up to 1GiB whenever there is less than 100MiB left.
+    extraOptions = ''
+      min-free = ${toString (100 * 1024 * 1024)}
+      max-free = ${toString (1024 * 1024 * 1024)}
+    '';
+  };
+
   users = {
     mutableUsers = false;
     users = {
