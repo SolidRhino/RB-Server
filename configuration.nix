0513279e4551785f16f60bcd47ac11ae3e0e28c1
SolidRhino <SolidRhino@users.noreply.github.com>
2023-05-14 20:51:44 +0200

    Add substituters

--- configuration.nix
+++ configuration.nix
@@ -20,6 +20,16 @@
     settings = {
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+        "https://nixos-pi.cachix.org/"
+      ];
+
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nixos-pi.cachix.org-1:SPIYe50yOaVAHLETuPoKnfPXrB0/ADlG2lwCve0MXoo="
+      ];
     };
     gc = {
       automatic = true;
